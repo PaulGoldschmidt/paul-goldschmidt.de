dce01dcb0b0f2a40f2b03171fc77e445a0c49050
Paul Goldschmidt <mail@paul-goldschmidt.de>
2026-03-27 10:53:28 +0100

    add privacy policy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
+  redirects: {
+    "/impressum": "/imprint",
+    "/datenschutz": "/privacy",
+  },
 });
