7e238d3e8ff63b96c24891eab1b73def34e8fc5f
Paul Goldschmidt <mail@paul-goldschmidt.de>
2026-03-29 01:07:32 +0100

    SEO+GEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://paul-goldschmidt.de",
   output: "static",
+  integrations: [sitemap()],
   redirects: {
     "/impressum": "/imprint",
     "/datenschutz": "/privacy",
