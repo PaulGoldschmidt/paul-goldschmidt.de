0a22255d1b8799e4222030b04a2437b00f8b973b
Paul Goldschmidt <mail@paul-goldschmidt.de>
2026-03-31 17:28:20 +0200

    inlineStylesheets: "always"

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://paul-goldschmidt.de",
   output: "static",
+  build: {
+    inlineStylesheets: "always",
+  },
   integrations: [sitemap()],
   redirects: {
     "/impressum": "/imprint",
