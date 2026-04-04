20dc9e450020417de0130e2435998498419e9760
Paul Goldschmidt <mail@paul-goldschmidt.de>
2026-04-04 11:40:33 +0200

    add english blog option

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,7 @@ export default defineConfig({
     "/instagram": "https://www.instagram.com/_paulgoldschmidt/",
     "/twitter": "https://twitter.com/PauIGoldschmidt",
     "/blog": "https://p3g3.de/",
+    "/blog-en": "https://blog.paul-goldschmidt.de/",
     "/test": "https://youtu.be/dQw4w9WgXcQ",
   },
 });
